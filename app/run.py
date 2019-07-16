3a808a5ee65bed5752ce2c6e9e3293d4d4112344
Strohecker Daniel <daniel.strohecker@mobi.ch>
2019-07-16 11:52:56 +0200

    flake stuff

--- app/run.py
+++ app/run.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, jsonify, request
-from flask_restplus import Api, Resource, reqparse
+from flask import Flask, request, render_template, Markup
+from flask_restplus import Api, Resource
 from libs.sqlite import SQLService
 from pybadges import badge as badgeGen
 
 
 import sys
 import logging
-import time
 logging.basicConfig(stream=sys.stdout,
                     level=logging.INFO,
                     format="%(asctime)s %(levelname)-5s: %(message)s")
@@ -21,6 +20,7 @@ init = SQLService()
 init.createTable()
 del init
 
+
 @api.route("/")
 class CreateBadge(Resource):
 
@@ -31,10 +31,10 @@ class CreateBadge(Resource):
         badge = SQLService()
         badge.generateBadge()
 
-        getUrl = "%s%s" % (request.url_root, 
+        getUrl = "%s%s" % (request.url_root,
                            badge.badgeId)
-        putUrl = "%s%s?token=%s" % (request.url_root, 
-                                    badge.badgeId, 
+        putUrl = "%s%s?token=%s" % (request.url_root,
+                                    badge.badgeId,
                                     badge.token)
         return {"get_url": getUrl, "put_url": putUrl}, 200
 
@@ -49,12 +49,12 @@ class UseBadge(Resource):
         """
         badge = SQLService()
         badge.getBadge()
-        svg = badgeGen(left_text=badge.name, 
-                       right_text=badge.value, 
+        svg = badgeGen(left_text=badge.name,
+                       right_text=badge.value,
                        left_color=badge.name_color,
                        right_color=badge.value_color)
-        
-        return render_template("svg.jinja", svg = Markup(svg))
+
+        return render_template("svg.jinja", svg=Markup(svg))
 
     @api.doc(params={'id': 'An ID', "token": "Access token"})
     @api.doc(responses={403: 'Not Authorized'})
@@ -62,16 +62,16 @@ class UseBadge(Resource):
         """
         Changes the Badge on the ID with Token
         """
-        token  = request.form['token']  if "token"  in request.form else None
-        name   = request.form['name']   if "name"   in request.form else None
-        value  = request.form['value']  if "value"  in request.form else None
+        token = request.form['token']if "token" in request.form else None
+        name = request.form['name'] if "name" in request.form else None
+        value = request.form['value'] if "value" in request.form else None
         ncolor = request.form['ncolor'] if "ncolor" in request.form else None
         vcolor = request.form['vcolor'] if "vcolor" in request.form else None
 
         badge = SQLService(id, token)
-        if not badge.getBadge(): api.abort(403)
+        if not badge.getBadge():
+            api.abort(403)
         badge.updateBadge(name, value, ncolor, vcolor)
-    
         return {}
 
 
